7e30d15a416c2e40000913f4920ce18d87304d7c
zovt <zovt@posteo.de>
2018-10-31 21:28:39 -0400

    Basic def

--- interpreter/newt.scm
+++ interpreter/newt.scm
@@ -6,70 +6,77 @@
 (define-record const value)
 
 ; bindings are an alist of symbol names to a binding
-; a binding is one of: (Lang Proc Type Const)
+; a binding is one of: (Lang proc Type Const)
 (define-record context bindings)
+(define-record-printer (context con out)
+  (fprintf out "#.(context ~a)" (context-bindings con)))
+
+(define (handle-def name val context)
+  (make-context (append (context-bindings context) `((,name . ,(make-const val))))))
 
 (define builtins 
   `(;; functions
     (println . ,(make-proc (lambda (thing) (display thing) (newline))))
-    (+ . ,(make-proc (lambda (a b) (+ a b))))
+    (+ . ,(make-proc +))
     ;; types
     (int . ,(make-type 'int))
     ;; language features
-    (def . ,(make-lang
-	      (lambda (name val context)
-		(make-context (append (context-bindings context) '((,name ,val)))))))
+    (def . ,(make-lang handle-def))
     (sig . ,(make-lang (lambda (name val context) context)))))
 
 (define (default-context)
   (make-context builtins))
 
+(define (eval-lang form-name-binding form-rest context)
+  (apply (lang-handler form-name-binding) (append form-rest (list context))))
+
+(define (eval-proc form-name-binding form-rest context)
+  (display 'eval-proc)
+  (newline)
+  (display form-rest)
+  (newline)
+  (apply (proc-fn form-name-binding) form-rest))
+
 (define (eval-form form context)
-  (display "eval-form")
-  (newline)
-  (display form)
-  (newline)
   (let* ([form-name (car form)]
-	 [form-rest (cdr form)]
-	 [form-name-binding (assoc form-name (context-bindings context))]
-	 [form-rest-eval (map (lambda (item) (car (evaluate item context))) form-rest)])
-    (if (or (null? form-name-binding) (eq? #f form-name-binding))
-	(error (string-append (symbol->string form-name) " is not found in the context"))
-	(cond [(proc? form-name-binding)
-	       (let ([fn (proc-fn form-name-binding)])
-		 (if (eq? (length form-rest-eval)
-			  (length (cdr (procedure-information fn))))
-		     `(,(apply fn form-rest-eval) ,context)
-		     (error "Missing required args")))]))))
+         [form-rest (cdr form)]
+         [form-name-binding-entry (assoc form-name (context-bindings context))])
+    (if (or (null? form-name-binding-entry) (eq? #f form-name-binding-entry))
+        (error (string-append (symbol->string form-name) " is not found in the context"))
+        (let ([form-name-binding (cdr form-name-binding-entry)])
+          (cond [(lang? form-name-binding)
+                 `(() . ,(eval-lang form-name-binding form-rest context))]
+                [(proc? form-name-binding)
+                 `(,(eval-proc form-name-binding (map (lambda (item) (car (evaluate item context))) form-rest) context) . ,context)]
+                [(const? form-name-binding)
+                 `(,(const-value form-name-binding) . ,context)]
+                )))))
 
 (define (eval-binding item context)
-  (display "Called eval-binding")
-  (newline)
-  (display item)
-  (newline)
-  `(,item ,context))
+  (let ([binding (assoc item (context-bindings context))])
+    (cond [(or (null? binding) (eq? #f binding))
+           (error (format #f "The symbol ~a is not defined" item))]
+          [(not (const? (cdr binding))) (error (format #f "The symbol ~a is not a const" item))]
+          [else `(,(const-value (cdr binding)) . ,context)])))
 
 (define (evaluate item context)
-  (display "evaluate")
-  (newline)
-  (display item)
-  (newline)
   (cond [(list? item) (eval-form item context)]
-	[(symbol? item) (eval-binding item context)]
-	[(string? item) '(,item ,context)]
-	[(number? item) '(,item ,context)]
-	[else (error (string-append (format #f "~a" item) " is not list, symbol, or string"))]))
-
+        [(symbol? item) (eval-binding item context)]
+        [(string? item) `(,item . ,context)]
+        [(number? item) `(,item . ,context)]
+        [else (error (string-append (format #f "~a" item) " is not list, symbol, or string"))]))
 (define (run file-path)
   (let [(context (default-context))
-	(forms (read-file file-path))]
+        (forms (read-file file-path))]
     (foldl (lambda (context form)
-	     (let ([out (evaluate form context)])
-	       (display (car out))
-	       (newline)
-	       (cdr out)))
-	   context
-	   forms)))
+       (display form)
+       (newline)
+       (let ([out (evaluate form context)])
+         (display (car out))
+         (newline)
+         (cdr out)))
+     context
+     forms)))
 
 (if (null? (command-line-arguments))
     (display "Missing file to run\n")
